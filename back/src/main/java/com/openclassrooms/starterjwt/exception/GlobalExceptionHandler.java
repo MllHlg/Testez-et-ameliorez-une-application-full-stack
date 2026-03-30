d643809efdb346956bc9ebc6b3944d8832face09
MllHlg <maelle772001@gmail.com>
2026-03-30 15:09:56 +0200

    Refactor: centralisation de la gestion des exceptions.

---
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.starterjwt.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<?> handleNumberFormatException(NumberFormatException ex) {
+        return ResponseEntity.badRequest().build();
+    }
+}
